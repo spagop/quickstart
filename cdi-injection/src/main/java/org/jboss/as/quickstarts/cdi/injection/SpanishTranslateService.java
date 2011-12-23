jboss-developer/jboss-eap-quickstarts

Creating the CDI injection quick start

helloworld-translate/src/main/java/org/jboss/as/quickstarts/helloworld/translate/SpanishTranslateService.java
@@ -1,0 +1,21 @@
+package org.jboss.as.quickstarts.helloworld.translate;
+
+import org.jboss.as.quickstarts.helloworld.translate.qualifier.Spanish;
+
+/**
+ * A simple implementation for the Spanish language.
+ *
+ * The {@link Spanish} qualifier tells CDI this is a special instance of
+ * the {@link TranslateService}.
+ *
+ * @author Jason Porter
+ */
+@Spanish
+public class SpanishTranslateService implements TranslateService {
+
+   @Override
+   public String hello() {
+     return "Hola";
+   }
+
+}

helloworld-translate/src/main/java/org/jboss/as/quickstarts/helloworld/translate/TranslateService.java
@@ -1,0 +1,12 @@
+package org.jboss.as.quickstarts.helloworld.translate;
+
+/**
+ * A simple interface which will define translation phrases supported by the system.
+ *
+ * @author Jason Porter
+ */
+public interface TranslateService {
+
+   String hello();
+
+}

helloworld-translate/src/main/java/org/jboss/as/quickstarts/helloworld/translate/qualifier/Spanish.java
@@ -1,0 +1,26 @@
+package org.jboss.as.quickstarts.helloworld.translate.qualifier;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+/**
+ * Qualifier used to help determine the intended injection object.
+ *
+ * Qualifiers are special annotations created in a CDI application. The {@link Qualifier} annotation
+ * specifies this as a CDI qualifier, which is used during injection to narrow the intended type
+ * to be injected.
+ *
+ * This particular qualifier will be used to specify types which should be used for the Spanish language.
+ * @author Jason Porter
+ */
+@Qualifier
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.PARAMETER})
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Spanish {
+}

spagop/quickstart

cdi-injection/src/main/java/org/jboss/as/quickstarts/cdi/injection/SpanishTranslateService.java
@@ -1,0 +1,21 @@
+package org.jboss.as.quickstarts.cdi.injection;
+
+import org.jboss.as.quickstarts.cdi.injection.qualifier.Spanish;
+
+/**
+ * A simple implementation for the Spanish language.
+ *
+ * The {@link Spanish} qualifier tells CDI this is a special instance of
+ * the {@link TranslateService}.
+ *
+ * @author Jason Porter
+ */
+@Spanish
+public class SpanishTranslateService implements TranslateService {
+
+   @Override
+   public String hello() {
+     return "Hola";
+   }
+
+}

cdi-injection/src/main/java/org/jboss/as/quickstarts/cdi/injection/TranslateService.java
@@ -1,0 +1,12 @@
+package org.jboss.as.quickstarts.cdi.injection;
+
+/**
+ * A simple interface which will define translation phrases supported by the system.
+ *
+ * @author Jason Porter
+ */
+public interface TranslateService {
+
+   String hello();
+
+}

cdi-injection/src/main/java/org/jboss/as/quickstarts/cdi/injection/qualifier/Spanish.java
@@ -1,0 +1,26 @@
+package org.jboss.as.quickstarts.cdi.injection.qualifier;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+/**
+ * Qualifier used to help determine the intended injection object.
+ *
+ * Qualifiers are special annotations created in a CDI application. The {@link Qualifier} annotation
+ * specifies this as a CDI qualifier, which is used during injection to narrow the intended type
+ * to be injected.
+ *
+ * This particular qualifier will be used to specify types which should be used for the Spanish language.
+ * @author Jason Porter
+ */
+@Qualifier
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.PARAMETER})
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Spanish {
+}