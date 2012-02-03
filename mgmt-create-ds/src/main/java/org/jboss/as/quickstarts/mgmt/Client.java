ae33869b2230bb6c39c7f60955892781f7ca2084
Juergen Hoffmann <jhoffmann@redhat.com>
2012-02-03 15:42:55 +0100

    further enhancement to the creation process.
    Did some further enhancement to the creation process. I still need to
    wrap this into properties. And to 1st check for installed drivers and
    reusing them.

--- mgmt-create-ds/src/main/java/org/jboss/as/quickstarts/mgmt/Client.java
+++ mgmt-create-ds/src/main/java/org/jboss/as/quickstarts/mgmt/Client.java
@@ -1,95 +1,84 @@
 package org.jboss.as.quickstarts.mgmt;
 
 import java.net.InetAddress;
-import java.util.List;
 
 import org.jboss.as.controller.client.ModelControllerClient;
 import org.jboss.dmr.ModelNode;
+import org.jboss.dmr.Property;
 
 /**
  * <p>
- * This example shows you how you can use the JBoss Application Server Management API to get the
- * Status of a standalone server."
+ * This example shows you how you can use the JBoss Application Server Management API to create a
+ * Datasource.
  * </p>
- * <p> The Model description via the CLI will look like this
- * <pre>
- * [standalone@localhost:9999 /] :read-attribute(name=server-state)
- * {
- *   "outcome" => "success",
- *   "result" => "running"
- * }
- * </pre>
- * </p>
- * 
- * @author Serge Pagop (spagop@redhat.com)
- * 
+ *
+ * @author Juergen Hoffmann (jhoffmann@redhat.com)
  */
-public class Client {
-	public static void main(String[] args) throws Exception {
-		ModelControllerClient client = ModelControllerClient.Factory.create(
-				InetAddress.getByName("localhost"), 9999);
-		try {
-			String dsname = "REPLACE_WITH_PROPERTY";
-			ModelNode op = new ModelNode();
-			op.get("operation").set("add");
+public class Client
+{
+    public static void main(String[] args) throws Exception
+    {
+        ModelControllerClient client = ModelControllerClient.Factory.create(
+                InetAddress.getByName("localhost"), 9999);
+        try
+        {
+            String dsname = "REPLACE_WITH_PROPERTY";
+            ModelNode op = new ModelNode();
+            op.get("operation").set("add");
 
-            debug("1", op);
+            ModelNode address = op.get("address");
+            address.add("subsystem", "datasources");
+            address.add("data-source", dsname);
 
-			ModelNode address = op.get("address");
-			address.add("subsystem", "datasources");
-			address.add("data-source", dsname);
+            op.get("jndi-name").set("java:jboss/datasources/" + dsname);
+            op.get("driver-name").set("mysql");
+            op.get("enabled").set(true);
+            op.get("pool-name").set("TestDS");
+            op.get("connection-url").set("jdbc:mysql://localhost/akdb");
 
-            debug("2", address);
+            ModelNode result = client.execute(op);
 
-			op.get("jndi-name").set("java:jboss/datasources/" + dsname);
-			op.get("driver-name").set("mysql");
-			op.get("enabled").set(true);
-			op.get("pool-name").set("TestDS");
-			op.get("connection-url").set("jdbc:mysql://localhost/akdb");
+            if (result.hasDefined("outcome")
+                    && "success".equals(result.get("outcome").asString()))
+            {
 
-			ModelNode result = client.execute(op);
+                ModelNode readOp = new ModelNode();
+                readOp.get("operation").set("read-resource");
+                readOp.get("address").add("subsystem", "datasources");
+                readOp.get("recursive").set(true);
 
-            debug("3", result);
-            System.out.println(result.toString());
+                ModelNode localResult = client.execute(readOp);
 
+                if (localResult.hasDefined("outcome") && "success".equals(localResult.get("outcome").asString()))
+                {
+                    System.out.println("SUCCESS!!!");
+                    System.out.println("The following Datasources are now configured");
 
-			if (result.hasDefined("outcome")
-					&& "success".equals(result.get("outcome").asString())) {
-				if (result.hasDefined("result")) {
-
-                    ModelNode readOp = new ModelNode();
-                    readOp.get("operation").set("read-resource");
-                    readOp.get("name").set("data-source");
-                    readOp.get("recursive").set(true);
-
-                    ModelNode localResult = client.execute(readOp);
-
-                    System.out.println(localResult.toString());
-
-                    List<ModelNode> results = localResult.get("result").asList();
-                    for (ModelNode oneResult : results)
+                    ModelNode dsList = localResult.get("result").get("data-source");
+                    for (ModelNode ds : dsList.asList())
                     {
-                        debug("4", oneResult);
-                        System.out.println();
-                        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
-                        System.out.println("&&&&&& Datasource: " + oneResult.toString());
-                        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
-                        System.out.println();
-
+                        System.out.println("-------> Datasource: " + ds.asProperty().getName() + " <------");
+                        for (Property prop : ds.asProperty().getValue().asPropertyList())
+                        {
+                            System.out.println(prop.getName() + "=" + prop.getValue());
+                        }
                     }
-				}
-			} else if (result.hasDefined("failure-description")) {
-				throw new RuntimeException(result.get("failure-description")
-						.toString());
-			} else {
-				throw new RuntimeException(
-						"Operation not successful; outcome = "
-								+ result.get("outcome"));
-			}
-		} finally {
-			client.close();
-		}
-	}
+                }
+            } else if (result.hasDefined("failure-description"))
+            {
+                throw new RuntimeException(result.get("failure-description")
+                        .toString());
+            } else
+            {
+                throw new RuntimeException(
+                        "Operation not successful; outcome = "
+                                + result.get("outcome"));
+            }
+        } finally
+        {
+            client.close();
+        }
+    }
 
     private static void debug(String identifier, ModelNode node)
     {
