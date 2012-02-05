5f8287eb811bda60ccc587a53ed9495b728075d7
Juergen Hoffmann <jhoffmann@redhat.com>
2012-02-05 10:10:17 +0100

    - Moved the Files to the right Position for maven calls. - updated the README to document how this quickstart is called

--- mgmt-create-jms-queue/src/main/java/org/jboss/as/quickstarts/mgmt/DemoAuthentication.java
+++ mgmt-create-jms-queue/src/main/java/org/jboss/as/quickstarts/mgmt/DemoAuthentication.java
@@ -1,4 +1,4 @@
-package mgmt;
+package org.jboss.as.quickstarts.mgmt;
 
 
 import javax.security.auth.callback.*;
@@ -14,7 +14,8 @@ import java.net.PasswordAuthentication;
  *
  * @author <a href="mailto:darran.lofthouse@jboss.com">Darran Lofthouse</a>
  */
-public class DemoAuthentication {
+public class DemoAuthentication
+{
 
     private static final DemoAuthentication INSTANCE = new DemoAuthentication();
 
@@ -28,69 +29,87 @@ public class DemoAuthentication {
     private String userName = null;
     private char[] password = null;
 
-    private DemoAuthentication() {
+    private DemoAuthentication()
+    {
     }
 
-    public static CallbackHandler getCallbackHandler() {
+    public static CallbackHandler getCallbackHandler()
+    {
         return INSTANCE.CALLBACK_HANDLER;
     }
 
-    public static Authenticator getAuthenticator() {
+    public static Authenticator getAuthenticator()
+    {
         return INSTANCE.AUTHENTICATOR;
     }
 
-    void prompt(final String realm) {
-        if (promptShown == false) {
+    void prompt(final String realm)
+    {
+        if (promptShown == false)
+        {
             promptShown = true;
             System.out.println("Authenticating against security realm: " + realm);
         }
     }
 
-    String obtainUsername(final String prompt) {
-        if (userName == null) {
+    String obtainUsername(final String prompt)
+    {
+        if (userName == null)
+        {
             userName = System.console().readLine(prompt);
         }
         return userName;
     }
 
-    char[] obtainPassword(final String prompt) {
-        if (password == null) {
+    char[] obtainPassword(final String prompt)
+    {
+        if (password == null)
+        {
             password = System.console().readPassword(prompt);
         }
 
         return password;
     }
 
-    class DemoCallbackHandler implements CallbackHandler {
+    class DemoCallbackHandler implements CallbackHandler
+    {
 
-        public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+        public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException
+        {
 
             // Special case for anonymous authentication to avoid prompting user for their name.
-            if (callbacks.length == 1 && callbacks[0] instanceof NameCallback) {
+            if (callbacks.length == 1 && callbacks[0] instanceof NameCallback)
+            {
                 ((NameCallback) callbacks[0]).setName("anonymous demo user");
                 return;
             }
 
-            for (Callback current : callbacks) {
-                if (current instanceof RealmCallback) {
+            for (Callback current : callbacks)
+            {
+                if (current instanceof RealmCallback)
+                {
                     RealmCallback rcb = (RealmCallback) current;
                     String defaultText = rcb.getDefaultText();
                     rcb.setText(defaultText); // For now just use the realm suggested.
 
                     prompt(defaultText);
-                } else if (current instanceof RealmChoiceCallback) {
+                } else if (current instanceof RealmChoiceCallback)
+                {
                     throw new UnsupportedCallbackException(current, "Realm choice not currently supported.");
-                } else if (current instanceof NameCallback) {
+                } else if (current instanceof NameCallback)
+                {
                     NameCallback ncb = (NameCallback) current;
                     String userName = obtainUsername("Username:");
 
                     ncb.setName(userName);
-                } else if (current instanceof PasswordCallback) {
+                } else if (current instanceof PasswordCallback)
+                {
                     PasswordCallback pcb = (PasswordCallback) current;
                     char[] password = obtainPassword("Password:");
 
                     pcb.setPassword(password);
-                } else {
+                } else
+                {
                     throw new UnsupportedCallbackException(current);
                 }
 
@@ -99,9 +118,11 @@ public class DemoAuthentication {
 
     }
 
-    class DemoAuthenticator extends Authenticator {
+    class DemoAuthenticator extends Authenticator
+    {
         @Override
-        protected PasswordAuthentication getPasswordAuthentication() {
+        protected PasswordAuthentication getPasswordAuthentication()
+        {
             prompt(getRequestingPrompt());
             String userName = obtainUsername("Username:");
             char[] password = obtainPassword("Password:");
