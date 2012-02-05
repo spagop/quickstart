9dba6d135c272e5990c3cba8d7fb083cb12667f6
Juergen Hoffmann <jhoffmann@redhat.com>
2012-02-05 01:13:02 +0100

    little changes

--- mgmt-create-jms-queue/src/main/java/mgmt/Client.java
+++ mgmt-create-jms-queue/src/main/java/mgmt/Client.java
@@ -1,15 +1,15 @@
-package org.jboss.as.quickstarts.mgmt;
-
-import java.net.InetAddress;
+package mgmt;
 
 import org.jboss.as.controller.client.ModelControllerClient;
 import org.jboss.dmr.ModelNode;
 import org.jboss.dmr.Property;
 
+import java.net.InetAddress;
+
 /**
  * <p>
  * This example shows you how you can use the JBoss Application Server Management API to create a
- * Datasource.
+ * JMS Queue.
  * </p>
  *
  * @author Juergen Hoffmann (jhoffmann@redhat.com)
@@ -20,21 +20,20 @@ public class Client
     {
         ModelControllerClient client = ModelControllerClient.Factory.create(
                 InetAddress.getByName("localhost"), 9999);
+
+        ModelNode result;
+
         try
         {
-            String dsname = "REPLACE_WITH_PROPERTY";
+            String queueName = "REPLACE_WITH_PROPERTY";
             ModelNode op = new ModelNode();
             op.get("operation").set("add");
 
-            op.get("address").add("subsystem", "datasources").add("data-source", dsname);
+            op.get("address").add("subsystem", "messaging").add("hornetq-server", "default").add("jms-queue", queueName);
+            op.get("entries").add("queue/" + queueName);
+            op.get("durable").set(true);
 
-            op.get("jndi-name").set("java:jboss/datasources/" + dsname);
-            op.get("driver-name").set("mysql");
-            op.get("enabled").set(true);
-            op.get("pool-name").set("TestDS");
-            op.get("connection-url").set("jdbc:mysql://localhost/akdb");
-
-            ModelNode result = client.execute(op);
+            result = client.execute(op);
 
             if (result.hasDefined("outcome")
                     && "success".equals(result.get("outcome").asString()))
@@ -42,21 +41,21 @@ public class Client
 
                 ModelNode readOp = new ModelNode();
                 readOp.get("operation").set("read-resource");
-                readOp.get("address").add("subsystem", "datasources");
+                readOp.get("address").add("subsystem", "messaging").add("hornetq-server", "default");
                 readOp.get("recursive").set(true);
 
-                ModelNode localResult = client.execute(readOp);
+                 result = client.execute(readOp);
 
-                if (localResult.hasDefined("outcome") && "success".equals(localResult.get("outcome").asString()))
+                if (result.hasDefined("outcome") && "success".equals(result.get("outcome").asString()))
                 {
                     System.out.println("SUCCESS!!!");
-                    System.out.println("The following Datasources are now configured");
+                    System.out.println("The following JMS Queues are now configured");
 
-                    ModelNode dsList = localResult.get("result").get("data-source");
-                    for (ModelNode ds : dsList.asList())
+                    ModelNode queueList = result.get("result").get("jms-queue");
+                    for (ModelNode queue : queueList.asList())
                     {
-                        System.out.println("-------> Datasource: " + ds.asProperty().getName() + " <------");
-                        for (Property prop : ds.asProperty().getValue().asPropertyList())
+                        System.out.println("-------> JMS Queue: " + queue.asProperty().getName() + " <------");
+                        for (Property prop : queue.asProperty().getValue().asPropertyList())
                         {
                             System.out.println(prop.getName() + "=" + prop.getValue());
                         }
